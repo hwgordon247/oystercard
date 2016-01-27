22da7f7a6f506450976168ab52d740c8af5d67b2
Henry Gordon <hwgordon247@gmail.com>
2016-01-27 11:56:45 +0000

    introducing journey calss

---
+++ lib/journey.rb
@@ -0,0 +1,26 @@
+require_relative 'station.rb'
+
+class Journey
+  attr_reader :record_journey,
+
+  def initialize(entry_station)
+    @record_journey = {}
+    @entry_station = entry_station
+    #start_journey(entry_station)
+  end
+
+  def end_journey(exit_station)
+    @entry_station = nil
+    @record_journey[:exit] = exit_station
+  end
+
+  def fare
+  end
+
+
+
+  def start_journey(entry_station)
+  	@record_journey[:entry] = entry_station
+  end
+
+end
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,13 @@
 
 class Oystercard
 
-  attr_reader :balance, :journey_history, :record_journey, :entry_station, :exit_station
+  attr_reader :balance, :journey_history, :journey
 
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
-    @record_journey = {}
     @journey_history = []
   end
 
@@ -19,22 +18,17 @@ class Oystercard
 
   def touch_in(entry_station)
   	raise "Insufficient funds" if @balance < MINIMUM_FARE
-    @entry_station = entry_station
-  	@record_journey[:entry] = entry_station
+    @journey = Journey.new entry_station
+    #@journey.start_journey(entry_station)
   end
 
   def touch_out(exit_station)
     deduct(MINIMUM_FARE)
-    @exit_station = exit_station
-    @entry_station = nil
-    @record_journey[:exit] = exit_station
-    journey_history << record_journey
+    @journey.end_journey(exit_station)
+    journey_history << @journey.record_journey
     record_journey = {}
   end
 
-  def in_journey?
-  	!!entry_station
-  end
 
   private
 
